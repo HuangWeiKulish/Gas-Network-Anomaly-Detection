36e4bc5d692b7fc26d9d9e8e35de113582cc7e61
HuangWeiKulish <36748466+HuangWeiKulish@users.noreply.github.com>
2019-09-16 16:07:38 +0800

    Add files via upload

--- Graph_AutoEncoder.py
+++ Graph_AutoEncoder.py
@@ -2,16 +2,16 @@ import dgl.function as dfn
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 gcn_msg = dfn.copy_src(src='h', out='m')  # message
 gcn_reduce = dfn.sum(msg='m', out='h')  # reduce function
 
 
-class GCNLayer(nn.Module):  # Graph Convolution Layer
+class TGCNCell(nn.Module):  # Graph Convolution Layer
 
-    def __init__(self, in_feats, out_feats, dropout=0.0, activf=F.relu):
-        super(GCNLayer, self).__init__()
-        self.linear = nn.Linear(in_feats, out_feats)
+    def __init__(self, in_feats, out_feats, dropout=0.0, activf=F.relu, bias=True):
+        super(TGCNCell, self).__init__()
+        self.temporal_cell = nn.LSTMCell(in_feats, out_feats, bias)  # input as (seq_len, batch, input_size), unless set bach_first=True then input use (batch, seq_len, input_size)
+        #self.linear = nn.Linear(in_feats, out_feats)
         self.dropout = dropout  # outputs are scaled by (1/(1-dropout)), default value in F.dropout is 0.5
         self.activf = activf
 
@@ -32,26 +32,26 @@ class GCNLayer(nn.Module):  # Graph Convolution Layer
         g.recv(g.nodes(), gcn_reduce)
         # get the result node features
         h = g.ndata.pop('h')  # return value of key 'h' and remove this item from dictionary g.ndata
-        # perform linear transformation
-        out = self.linear(h)
+        # perform temporal transformation
+        out = self.temporal_cell(h)  #out = nn.linear(h)
         # activate
         out = self.activf(out)
         return out
 
 
-class GCNAE(nn.Module):  # Graph Auto-Encoder
+class TGCNAE(nn.Module):  # Graph Auto-Encoder
 
-    def __init__(self, in_feats, gcn_out, hidden_dim_list, dropout, activf=F.relu):
+    def __init__(self, in_feats, tgcnout_feats, hidden_dim_list, dropout, activf=F.relu):
         """
         This is a symmetric auto-encoder
         :param in_feats:
         :param hidden_dim_list: a list where each element is the dimension of a hidden layer
         :param dropout:
         """
-        super(GCNAE, self).__init__()
+        super(TGCNAE, self).__init__()
         self.in_feats, self.hidden_dim_list, self.dropout, self.activf = in_feats, hidden_dim_list, dropout, activf
-        self.gcn_layer = GCNLayer(in_feats, gcn_out, dropout, activf)
-        self.encoding_dims = [gcn_out] + hidden_dim_list
+        self.tgcn_layer = TGCNCell(in_feats, tgcnout_feats, dropout, activf)
+        self.encoding_dims = [tgcnout_feats] + hidden_dim_list
         self.decoding_dims = self.encoding_dims[::-1]
         self.encoder = self.encode()
         self.decoder = self.decode()
@@ -60,21 +60,38 @@ class GCNAE(nn.Module):  # Graph Auto-Encoder
         layers = []
         n_layers = len(self.encoding_dims)
         for n in range(1, n_layers):
-            layers.append(nn.LSTM(input_size=self.in_feats, hidden_size=10, num_layers=1))
+            layers.append(nn.Linear(in_features=self.encoding_dims[n-1], out_features=self.encoding_dims[n], bias=True))
+            layers.append(nn.ELU(alpha=1.0, inplace=False))
         return nn.Sequential(*layers)
 
     def decode(self):
         layers = []
         n_layers = len(self.decoding_dims)
         for n in range(1, n_layers):
-            layers.append(nn.LSTM(self.decoding_dims[n-1], self.decoding_dims[n], self.dropout, self.activf))
+            layers.append(nn.Linear(in_features=self.decoding_dims[n-1], out_features=self.decoding_dims[n], bias=True))
+            layers.append(nn.ELU(alpha=1.0, inplace=False))
         return nn.Sequential(*layers)
 
-    def forward(self, g, input):
-        x = self.gcn_layer(g, input)
+    def forward(self, g, inputs):
+        x = self.tgcn_layer(g, inputs)
         x = self.encoder(x)
         x = self.decoder(x)
         return x
+
+
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # PyTorch v0.4.0
+in_feats = 3  # 2 time features and 1 pressure features
+tgcnout_feats = 1  # output 1 feature from tgcn cell
+hidden_dim_list = [20, 10]
+dropout = 0.1
+activf = F.relu
+
+gasnetwork_model = TGCNAE(in_feats, tgcnout_feats, hidden_dim_list, dropout, activf)
+print(gasnetwork_model)
+
+
+
+
 
 
 
